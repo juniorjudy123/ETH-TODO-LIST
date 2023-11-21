5c9f9725715ba3f9e766ff6d93426e3bb52fa42d
benoy_Judy <benoyjudy@gmail.com>
2023-11-21 11:44:00 +0530

    task list and client side application setup

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,7 +1,25 @@
 pragma solidity^0.5.0;
 
 contract TodoList{
-    uint public tasksCount=0; //state variable
+    uint public taskCount=0; //state variable
+    
+
+    struct Task{
+        uint id;
+        string content;
+        bool completed;
+        }
+
+     mapping(uint => Task) public tasks;
+     constructor()public{
+        createTask("check out website");
+     }
+
+     function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount , _content ,false);
+
+     }
 
 
 
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,3 +3,4 @@ var TodoList = artifacts.require("./TodoList.sol");
 module.exports = function (deployer) {
     deployer.deploy(TodoList);
 };
+
